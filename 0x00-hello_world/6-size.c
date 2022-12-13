e40abe42d6e84622147d647ec14462af9692a4cc
Odongo-4 <pius.nondi@gmail.com>
2022-12-13 07:58:11 -0800

    6-size.c_01:

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,6 @@ int main(void)
   printf("Size of long int: %d bytes (s)\n",sizeof(long int));
   printf("Size of long long int: %d bytes (s)\n",sizeof(long long int));
   printf("Size of a float: %d bytes (s)\n",sizeof(float));
-
   return (0);
 }
 
