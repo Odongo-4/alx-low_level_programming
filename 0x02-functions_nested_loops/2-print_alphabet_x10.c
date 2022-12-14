2f10c9f55bac0e04a4a0db6139e40569278c049e
Odongo-4 <pius.nondi@gmail.com>
2022-12-14 09:58:10 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alpha 10 times
+ */
+void print_alphabet_x10(void)
+{
+int ten = 0;
+char la;
+while (ten <= 9)
+{
+for (la = 'a'; la <= 'z'; la++)
+{
+_putchar(la);
+_putchar('\n');
+}
+ten++;
+}
+}
