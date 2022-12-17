30931a85f148540a5ef3873a6612c8230d38833f
Odongo-4 <pius.nondi@gmail.com>
2022-12-17 03:05:29 -0800

    7-print_diagonal.c_01

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,12 +5,17 @@
  */
 void print_diagonal(int n)
 {
-int i;
+int i, j;
 if (n > 0)
 {
 for (i = 1; i <= n; i++)
 {
+for (j = 1; j <= i; j++)
+{
+_putchar(' ');
+}
 _putchar(92);
+_putchar('\n');
 }
 _putchar('\n');
 }
