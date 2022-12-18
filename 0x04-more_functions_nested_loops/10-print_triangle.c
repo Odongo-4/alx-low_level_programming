bfb4af99de1395e2151d3daffa4f21a7870617f4
Odongo-4 <pius.nondi@gmail.com>
2022-12-18 02:46:01 -0800

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_triangle - printsa triangle followed by anew line
+ * @size: number of lines
+ */
+void print_triangle(int size)
+{
+int i, j;
+for (i = 0; i < size; i++)
+{
+for (j = 1; j < (size - 1); j++)
+_putchar(' ');
+for (j-- ; j < size; j++)
+_putchar('#');
+if (i < (size - 1))
+_putchar('\n');
+}
+_putchar('\n');
+}
