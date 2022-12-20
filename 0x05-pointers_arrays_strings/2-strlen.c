ea444298f7b3060acc36d542c3014b4325d98699
Odongo-4 <pius.nondi@gmail.com>
2022-12-20 04:51:51 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strlen - displays the length of a string
+ * @s: the caharacter to be returned
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+int index;
+for (index = 0; s[index] != '\0'; index++)
+return (index);
+}
