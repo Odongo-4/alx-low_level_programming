c42b1c1e58334a89dda46e962504c1e6e0f89d99
Odongo-4 <pius.nondi@gmail.com>
2022-12-23 08:42:35 -0800

    0-strcat.c-02

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  *  * _strcat - Function that concatenates two strings.
  *   * @dest: first string
@@ -8,16 +7,10 @@
  */
 char *_strcat(char *dest, char *src)
 {
-int length, j;
-length = 0;
-while (dest[length] != '\0')
-{
-length++;
-}
-for (j = 0; src[j] != '\0'; j++, length++)
-{
-dest[length] = src[j];
-}
-dest[length] = '\0';
+int index = 0, dest_len = 0;
+while (dest[index++])
+dest_len++;
+for (index = 0; src[index]; index++)
+dest[dest_len++] = src[index];
 return (dest);
 }
