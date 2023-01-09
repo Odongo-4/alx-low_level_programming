c5a6d578dc453513d88c1373cbc7f996043b9ece
Odongo-4 <pius.nondi@gmail.com>
2023-01-09 04:33:09 -0800

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - Print the name of the program
+ *@argc: Count arguments
+ *@argv: Arguments
+ *Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+int count;
+if (argc >= 1)
+{
+for (count = 0; count < argc; count++)
+{
+printf("%s\n", argv[count]);
+}
+}
+return (0);
+}
