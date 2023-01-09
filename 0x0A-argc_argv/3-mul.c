ffd30e50894c49a1f791c0460c137f02498e8164
Odongo-4 <pius.nondi@gmail.com>
2023-01-09 05:14:52 -0800

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *main - prints the multiplication of two integers
+ *@argc: argument count
+ *@argv: argument vector
+ *Return: 0 if it works, 1 if there is an ERROR
+ */
+int main(int argc, char *argv[])
+{
+int n, m, mult;
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+n = atoi(argv[1]);
+/*
+ *atoi() is a function that converts sting to integer
+ *and is stored in stdli.h header file
+ */
+m = atoi(argv[2]);
+mult = n *m;
+printf("%d\n", diff);
+return (0);
+}
